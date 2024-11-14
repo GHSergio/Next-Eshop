4ac605d78cbb0503156aabd95e06196692b3173b
Ming Hsu <n209202003@gmail.com>
2024-11-14 19:50:13 +0800

    modift deploy error-3

--- src/components/members/UserProfile.tsx
+++ src/components/members/UserProfile.tsx
@@ -9,7 +9,6 @@ const UserProfile: React.FC = () => {
   return (
     <div className="space-y-2">
       <h2 className="text-xl font-semibold mb-4">個人資料</h2>
-      <p>帳戶名稱： {userInfo?.name}</p>
       <p>電子郵件： {userInfo?.email}</p>
       <p>電話號碼： {userInfo?.phone}</p>
       <p>地址： {userInfo?.address}</p>
